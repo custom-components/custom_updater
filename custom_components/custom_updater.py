c1f64099368cbbb91f4d2162011400f193756b53
ludeeus <joasoe@gmail.com>
2018-08-04 13:24:52 +0200

    version bump

--- custom_components/custom_updater.py
+++ custom_components/custom_updater.py
@@ -15,7 +15,7 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import track_time_interval
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 
 _LOGGER = logging.getLogger(__name__)
 
