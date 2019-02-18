4c84919d13b31266e3bf2ceacffeb7742aba9429
ludeeus <joasoe@gmail.com>
2019-02-18 13:42:34 +0100

    Bump pyupdate

--- custom_components/custom_updater.py
+++ custom_components/custom_updater.py
@@ -14,11 +14,11 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers.event import async_track_time_interval
 
-VERSION = '4.1.0'
+VERSION = '4.1.1'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['pyupdate==1.0.0']
+REQUIREMENTS = ['pyupdate==1.0.1']
 
 CONF_TRACK = 'track'
 CONF_HIDE_SENSOR = 'hide_sensor'
