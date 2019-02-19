2a30ce71069225133482679064e2e1b29bd185ae
ludeeus <joasoe@gmail.com>
2019-02-19 13:40:59 +0100

    Adds super custom logic

--- custom_components/custom_updater.py
+++ custom_components/custom_updater.py
@@ -14,11 +14,11 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers.event import async_track_time_interval
 
-VERSION = '4.1.5'
+VERSION = '4.2.0'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['pyupdate==1.1.4']
+REQUIREMENTS = ['pyupdate==1.2.2']
 
 CONF_TRACK = 'track'
 CONF_HIDE_SENSOR = 'hide_sensor'
