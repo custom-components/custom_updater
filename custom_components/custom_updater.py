5eb77645457da7b23f1667512a1da6f5f0f1653c
ludeeus <joasoe@gmail.com>
2019-02-19 12:01:27 +0100

    Fix state issue when upgrading components

--- custom_components/custom_updater.py
+++ custom_components/custom_updater.py
@@ -14,11 +14,11 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers.event import async_track_time_interval
 
-VERSION = '4.1.4'
+VERSION = '4.1.5'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['pyupdate==1.1.3']
+REQUIREMENTS = ['pyupdate==1.1.4']
 
 CONF_TRACK = 'track'
 CONF_HIDE_SENSOR = 'hide_sensor'
