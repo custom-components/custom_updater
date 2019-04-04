435d955651b03a04a30ea8b663fbbd23120718cc
Joakim Sørensen <joasoe@gmail.com>
2019-04-04 17:36:42 +0200

    Version bump

--- custom_components/custom_updater.py
+++ custom_components/custom_updater.py
@@ -15,7 +15,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers.event import async_track_time_interval
 
-VERSION = '4.2.18'
+VERSION = '4.2.19'
 
 _LOGGER = logging.getLogger(__name__)
 
